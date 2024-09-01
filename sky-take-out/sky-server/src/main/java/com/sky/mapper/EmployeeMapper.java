482fe88577c1b34a0064c05409f81a988dfc4fa8
Zshuaixxx <z_20058@qq.com>
2024-09-01 18:39:25 +0800

    员工分页查询

--- sky-take-out/sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-take-out/sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,6 +1,9 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
+import com.sky.result.PageResult;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
@@ -24,4 +27,11 @@ public interface EmployeeMapper {
     @Insert("insert into employee(name, username, password, phone, sex, id_number, create_time, update_time, create_user, update_user)" +
             " VALUES (#{name},#{username},#{password},#{phone},#{sex},#{idNumber},#{createTime},#{updateTime},#{createUser},#{updateUser})")
     void addEmp(Employee employee);
+
+    /**
+     * 分页查询员工
+     * @param employeePageQueryDTO
+     * @return
+     */
+    Page<Employee> pageViewEmp(EmployeePageQueryDTO employeePageQueryDTO);
 }
