10c5036ce536b96ab228f7d37a3a547e4a411668
Zshuaixxx <z_20058@qq.com>
2024-09-01 17:18:20 +0800

    新增员工

--- sky-take-out/sky-server/src/main/java/com/sky/SkyApplication.java
+++ sky-take-out/sky-server/src/main/java/com/sky/SkyApplication.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement //开启注解方式的事务管理
 @Slf4j
 public class SkyApplication {
+//    TODO 添加记录操作日志  添加骑手端  商家骑手自动接单功能
     public static void main(String[] args) {
         SpringApplication.run(SkyApplication.class, args);
         log.info("server started");
