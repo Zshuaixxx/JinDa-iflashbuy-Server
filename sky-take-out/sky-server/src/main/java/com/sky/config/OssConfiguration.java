358a4c14094b8b714d75bc4c228254b76ec717da
Zshuaixxx <z_20058@qq.com>
2024-09-04 17:25:37 +0800

    图片上传阿里云OSS功能

---
+++ sky-take-out/sky-server/src/main/java/com/sky/config/OssConfiguration.java
@@ -0,0 +1,31 @@
+package com.sky.config;
+
+/**
+ * @author 帅的被人砍
+ * @create 2024-09-04 16:52
+ */
+
+import com.sky.properties.AliOssProperties;
+import com.sky.utils.AliOssUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 配置类，用于创建ALiOssUtL对象
+ */
+@Configuration
+@Slf4j
+public class OssConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean
+    public AliOssUtil ali0ssUtil(AliOssProperties aliOssProperties) {
+        log.info("开始创建阿里云文件上传工具类对象：{}",aliOssProperties);
+        return new AliOssUtil(aliOssProperties.getEndpoint(),
+                aliOssProperties.getAccessKeyId(),
+                aliOssProperties.getAccessKeySecret(),
+                aliOssProperties.getBucketName());
+    }
+}
---
+++ sky-take-out/sky-server/src/main/java/com/sky/controller/admin/CommonController.java
@@ -0,0 +1,51 @@
+package com.sky.controller.admin;
+
+/**
+ * @author 帅的被人砍
+ * @create 2024-09-04 17:03
+ */
+
+import com.sky.result.Result;
+import com.sky.utils.AliOssUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.UUID;
+
+/**
+ * 通用接口
+ */
+@RestController
+@Slf4j
+public class CommonController {
+
+    @Autowired
+    private AliOssUtil aliOssUtil;
+    /**
+     * 文件上传
+     * @param file
+     * @return
+     */
+    @PostMapping("/admin/common/upload")
+    public Result<String> uploadFile(MultipartFile file){
+        log.info("文件上传{}",file);
+
+        try {
+            //原始文件名
+            String originalFilename = file.getOriginalFilename();
+            //截取原始文件名的后缀dfdfdf.png
+            String extension = originalFilename.substring(originalFilename.lastIndexOf("."));
+            //构造新文件名称
+            String objectName = UUID.randomUUID().toString() + extension;
+            //文件的请求路径
+            String filePath = aliOssUtil.upload(file.getBytes(), objectName);
+            return Result.success(filePath);
+        } catch(Exception e) {
+            log.error("文件上传失败",e);
+            return Result.error("图片上传失败");
+        }
+    }
+}
