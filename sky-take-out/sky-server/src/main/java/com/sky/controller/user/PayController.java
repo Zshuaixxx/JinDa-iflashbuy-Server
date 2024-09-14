ca685ec8e2ee1162c25f72f9ba96fb5108d78d9c
Zshuaixxx <z_20058@qq.com>
2024-09-14 10:02:35 +0800

    商家端来单提醒功能

--- sky-take-out/sky-server/src/main/java/com/sky/controller/user/PayController.java
+++ sky-take-out/sky-server/src/main/java/com/sky/controller/user/PayController.java
@@ -1,12 +1,17 @@
 package com.sky.controller.user;
 
+import com.alibaba.fastjson.JSON;
 import com.sky.dto.OrdersPaymentDTO;
 import com.sky.mapper.OrderMapper;
 import com.sky.result.Result;
+import com.sky.websocket.WebSocketServer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author 帅的被人砍
@@ -17,6 +22,8 @@ public class PayController {
 
     @Autowired
     private OrderMapper orderMapper;
+    @Autowired
+    private WebSocketServer webSocketServer;
 
     /**
      * 订单支付   此处仅模拟支付  直接修改数据库对应订单状态
@@ -26,6 +33,15 @@ public class PayController {
     @PutMapping("/user/order/payment")
     public Result pay(@RequestBody OrdersPaymentDTO ordersPaymentDTO){
         orderMapper.updateOrderStatus(ordersPaymentDTO.getOrderNumber(),2);
+
+        //向管理端发送来单提醒
+        Map map = new HashMap();
+        map.put("type",1);//1表示来单提醒2表示客户催单
+        map.put("orderId",ordersPaymentDTO.getOrderNumber());
+        map.put("content","订单号："+ ordersPaymentDTO.getOrderNumber());
+        String json = JSON.toJSONString(map);
+        webSocketServer.sendToAllClient(json);
+
         return Result.success();
     }
 }
